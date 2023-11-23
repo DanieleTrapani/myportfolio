bda0ab86878f22402ce8f9b068f0bc1b38527655
Daniele Trapani <daniele.trapani96@gmail.com>
2023-11-23 22:19:04 +0100

    svelte new + basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import '../app.pcss';
+	import { page } from '$app/stores';
+	const pathname = $page.url.pathname;
+
+	const changeActive = (event: any) => {
+		if (event.target.classList.contains('active')) {
+			return;
+		} else {
+			const active = document.querySelector('.active');
+			active?.classList.remove('active');
+			active?.classList.add('inactive');
+			event.target.classList.add('active');
+		}
+	};
+</script>
+
+<header class="flex justify-center p-4">
+	<div class="flex gap-1 bg-gray-300 px-3 py-1 rounded-full">
+		<a
+			href="/"
+			class="header-link {pathname === '/' ? 'active' : 'inactive'}"
+			on:click={() => changeActive(event)}>About</a
+		>
+		<a
+			href="/projects"
+			class="header-link {pathname === '/projects' ? 'active' : 'inactive'}"
+			on:click={() => changeActive(event)}>Projects</a
+		>
+		<a
+			href="/contact"
+			class="header-link {pathname === '/contact' ? 'active' : 'inactive'}"
+			on:click={() => changeActive(event)}>Contact</a
+		>
+	</div>
+</header>
+<slot />
+
+<style lang="postcss">
+	.header-link {
+		@apply rounded-full px-3 py-1;
+	}
+	.active {
+		@apply bg-gray-200 font-bold;
+	}
+
+	.inactive {
+		@apply duration-300 hover:bg-gray-100;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<main class="px-16">
+	<div><h4>Hi! My name is <span class="font-bold text-6xl">Daniele.</span></h4></div>
+</main>
